f223356448c7db5d9ab7d206e46e77e9834f68fb
Amelia Kabor <akabor94@gmail.com>
2022-03-18 08:52:22 -0700

    updates to favicon, home.js, name of bank

--- src/Components/home.js
+++ src/Components/home.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { Card } from "react-bootstrap";
+import {Link} from "react-router-dom";
 import UserContext from "./context";
 import BankPicture from "./bank-icon.png";
 
@@ -7,10 +8,12 @@ function Home(){
   const ctx = React.useContext(UserContext);
   return(
     <Card style={{width: "18em", display:"block", marginLeft:"auto", marginRight:"auto"}}>
-      <Card.Header style={{background: "#7094b8", color:"white"}}>Bad Bank Landing Page</Card.Header>
+      <Card.Header style={{background: "#7094b8", color:"white"}}>Bank of Amelia</Card.Header>
       <Card.Body style={{background:"#d6e0eb"}}> 
-        <Card.Title>Welcome to the Bad Bank Landing Page</Card.Title>
-        <Card.Text>You can interact with this banking application. Start by creating an account.</Card.Text>
+        <Card.Title>Welcome to Bank of Amelia</Card.Title>
+        <Card.Text>You can interact with this banking application. Start by 
+        <Link to="/createaccount/"> creating an account.</Link>
+        </Card.Text>
         <Card.Img src={BankPicture} style={{width: "50%", display:"block", marginLeft: "auto", marginRight: "auto"}}/>
       </Card.Body>
     </Card>
--- src/Components/navbar.js
+++ src/Components/navbar.js
@@ -6,7 +6,7 @@ function NavBar() {
   return (
     <nav className="navbar navbar-expand-lg navbar-light bg-light">
       <div className="container-fluid">
-        <a className="navbar-brand" href="#">Bad Bank</a>
+        <a className="navbar-brand" href="#">BOA</a>
         <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
           <span className="navbar-toggler-icon"></span>
         </button>
